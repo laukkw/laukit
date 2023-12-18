20a6fcb23494e5f5ff46cfbeccda2e59a4bd3781
laukkw <1319339638@qq.com>
2023-12-18 19:30:08 +0800

    fix:fix

--- abi_encode.go
+++ abi_encode.go
@@ -1,18 +1,49 @@
 package laukit
 
 import (
-	"fmt"
-	"github.com/laukkw/kwstart/errors"
+    "fmt"
+    "github.com/ethereum/go-ethereum/accounts/abi"
+    "github.com/laukkw/kwstart/errors"
+    "strings"
 )
 
 // AbiCoder 将数据按类型 encode 返回bytes
 func AbiCoder(argTypes []string, argValues []interface{}) ([]byte, error) {
-	if len(argTypes) != len(argValues) {
-		return nil, errors.New("invalid arguments - types and values do not match")
-	}
-	args, err := buildArgumentsFromTypes(argTypes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to build abi: %v", err)
-	}
-	return args.Pack(argValues...)
+    if len(argTypes) != len(argValues) {
+        return nil, errors.New("invalid arguments - types and values do not match")
+    }
+    args, err := buildArgumentsFromTypes(argTypes)
+    if err != nil {
+        return nil, fmt.Errorf("failed to build abi: %v", err)
+    }
+    return args.Pack(argValues...)
+}
+
+func ParseABI(abiJSON string) (abi.ABI, error) {
+    parsed, err := abi.JSON(strings.NewReader(abiJSON))
+    if err != nil {
+        return abi.ABI{}, fmt.Errorf("unable to parse abi json: %w", err)
+    }
+    return parsed, nil
+}
+
+func MustParseABI(abiJSON string) abi.ABI {
+    parsed, err := ParseABI(abiJSON)
+    if err != nil {
+        panic(err)
+    }
+    return parsed
+}
+
+func EncodeInputData(abi abi.ABI, method string, args ...interface{}) ([]byte, error) {
+    m, ok := abi.Methods[method]
+    if !ok {
+        return nil, fmt.Errorf("contract method %s not found", method)
+    }
+    input, err := m.Inputs.Pack(args...)
+    if err != nil {
+        return nil, err
+    }
+    input = append(m.ID, input...)
+    return input, nil
 }
--- abi_encode_test.go
+++ abi_encode_test.go
@@ -1,16 +1,29 @@
 package laukit
 
 import (
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"math/big"
-	"testing"
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/common/hexutil"
+    "math/big"
+    "testing"
 )
 
 func TestAbiCoder(t *testing.T) {
-	result, err := AbiCoder([]string{"uint256", "uint256"},
-		[]interface{}{big.NewInt(1), big.NewInt(2)})
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(hexutil.Encode(result))
+    result, err := AbiCoder([]string{"uint256", "uint256"},
+        []interface{}{big.NewInt(1), big.NewInt(2)})
+    if err != nil {
+        t.Fatal(err)
+    }
+    t.Log(hexutil.Encode(result))
+}
+
+func TestMustParseAbi(t *testing.T) {
+    str := "[{\"inputs\":[],\"name\":\"AllowanceOverflow\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"AllowanceUnderflow\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"InsufficientAllowance\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"InsufficientBalance\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"InvalidPermit\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"PermitExpired\",\"type\":\"error\"},{\"inputs\":[],\"name\":\"TotalSupplyOverflow\",\"type\":\"error\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"},{\"inputs\":[],\"name\":\"DOMAIN_SEPARATOR\",\"outputs\":[{\"internalType\":\"bytes32\",\"name\":\"result\",\"type\":\"bytes32\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"result\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"result\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"internalType\":\"uint8\",\"name\":\"\",\"type\":\"uint8\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"}],\"name\":\"nonces\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"result\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"owner\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"spender\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"value\",\"type\":\"uint256\"},{\"internalType\":\"uint256\",\"name\":\"deadline\",\"type\":\"uint256\"},{\"internalType\":\"uint8\",\"name\":\"v\",\"type\":\"uint8\"},{\"internalType\":\"bytes32\",\"name\":\"r\",\"type\":\"bytes32\"},{\"internalType\":\"bytes32\",\"name\":\"s\",\"type\":\"bytes32\"}],\"name\":\"permit\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"result\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"from\",\"type\":\"address\"},{\"internalType\":\"address\",\"name\":\"to\",\"type\":\"address\"},{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+    abi := MustParseABI(str)
+
+    b, err := EncodeInputData(abi, "transfer", common.HexToAddress("0x00"), big.NewInt(1))
+    if err != nil {
+        t.Fatal(err)
+    }
+    t.Log(hexutil.Encode(b))
+
 }
